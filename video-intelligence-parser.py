a95597f5c3be9b05a2a9a7412b785cd9a42a1efc
Ming Ling <mingling@mingling-macbookpro.roam.corp.google.com>
2019-09-05 21:52:04 -0400

    added flag for person detection

--- video-intelligence-parser.py
+++ video-intelligence-parser.py
@@ -5,6 +5,7 @@ import re
 import io
 import os
 import logging
+import datetime
 import time
 from google.cloud import pubsub_v1
 
@@ -15,9 +16,12 @@ regexp_right = re.compile(r"right : ([^\s]+)")
 regexp_top = re.compile(r"top : ([^\s]+)")
 regexp_bottom = re.compile(r"bottom : ([^\s]+)")
 
-#create publisher
+# create publisher
 topic_name = "projects/{}/topics/{}".format(os.getenv("GCP_PROJECT"), os.getenv("PUB_SUB_TOPIC"))
 publisher = pubsub_v1.PublisherClient()
+
+# tracking message publish time
+last_zoom_event = None
 
 def video_intelligence_annotate(outputfile):
     with io.open(outputfile, 'r') as video_intelligence_output:
@@ -26,7 +30,7 @@ def video_intelligence_annotate(outputfile):
             line = video_intelligence_output.readline()
             if not line:
                 time.sleep(5)
-            # finds line that is an entity and read the next 5 lines to construct dictionary
+
             if "Entity description:" in line:
                 # process the next 5 lines
                 # assumes that each object detected will consume 5 lines for output. will need to udpate if changes
@@ -42,9 +46,12 @@ def video_intelligence_annotate(outputfile):
                 entity["bottom"] = regexp_bottom.search(bounding_box).group(1)
 
                 # flag a zoom event in pub/sub
-                if (entity["entity_desc"].lower() == "person" and  float(entity["confidence"]) > 0.70):
+                if (entity["entity_desc"].lower() == "person" and \
+                    float(entity["confidence"]) > 0.70 and \
+                    (last_zoom_event is None or last_zoom_event <= datetime.datetime.now() - datetime.timedelta(seconds=10 ))):
+                    print("zoom event {}".format(datetime.datetime.now()))
                     entity["zoom"] = True
-                    
+                    last_zoom_event = datetime.datetime.now()  
                 publish_topic(entity)
 
 def publish_topic(entity):
